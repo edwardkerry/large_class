773cd8a1ebe9bcc767d0a7db4d42674914a3249b
ed kerry <edwardkerry@notonthehighstreet.com>
2016-07-15 11:16:58 +0100

    Pushes employee attr_reader from super to subclass

--- job_item.rb
+++ job_item.rb
@@ -1,6 +1,6 @@
 class JobItem
 
-  attr_reader :quantity, :employee
+  attr_reader :quantity
 
   def initialize(unit_price, quantity, is_labor=false, employee=nil)
     @unit_price = unit_price
--- labor_item.rb
+++ labor_item.rb
@@ -1,5 +1,7 @@
 class LaborItem < JobItem
 
+  attr_reader :employee
+
   def initialize(quantity, employee)
     super(0, quantity, true, employee)
   end
